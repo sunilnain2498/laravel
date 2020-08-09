f363bf1b9cdfd6a235ba01398add751212360e55
sunilnain2498 <sunilnain2498@gmail.com>
2020-08-09 15:20:29 +0530

    now updated

---
+++ resources/views/demo.blade.php
@@ -0,0 +1,5 @@
+<h1> Welcome to demo page</h1>
+<p>ygfbrfifiuewbfowgh9w iuwrhjwfn uwehfjkwef iuwhfiuewbf iuuwhefiuwebf kwehfiu2bfd iuwhfuewjf</p>
+
+<h1> Welcome to demo page</h1>
+<p>ygfbrfifiuewbfowgh9w iuwrhjwfn uwehfjkwef iuwhfiuewbf iuuwhefiuwebf kwehfiu2bfd iuwhfuewjf</p>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,1 +1,2 @@
 <h1> Welcome to test page</h1>
+<p>ygfbrfifiuewbfowgh9w iuwrhjwfn uwehfjkwef iuwhfiuewbf iuuwhefiuwebf kwehfiu2bfd iuwhfuewjf</p>
